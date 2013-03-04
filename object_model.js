9bb095e003467e587594511dd2d639c11420c42c
Joe Cormier <jncormier24@gmail.com>
2013-03-04 16:13:24 -0500

    Started the object model

---
+++ object_model.js
@@ -0,0 +1,10 @@
+/**
+ * object_model.js
+ * Joe Cormier [03/04/13]
+ * The different object models for rodents revenge
+ * Objects: Board, Square, Player, Game
+ **/
+/********** Board Object Model **********/
+/********** Square Object Model **********/
+/********** Player Object Model **********/
+/********** Game Object Model **********/
